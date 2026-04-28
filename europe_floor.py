40082447df4ba913b8b3ee8eb87e9788df1e675a
suma.aithal <aithalsuma65@gmail.com>
2026-04-28 17:14:14 +0530

    Python day1

---
+++ europe_floor.py
@@ -0,0 +1,4 @@
+#convert european floor to US floor
+inp = input("Europe floor? ")
+usf = int(inp) + 1 #united states floor
+print("US floor", usf)
---
+++ pay.py
@@ -0,0 +1,6 @@
+hrs = input("enter hours:")
+hrs = float(hrs)
+rate = input("enter rate:")
+rate = float(rate)
+pay = hrs * rate
+print("pay:", pay)
