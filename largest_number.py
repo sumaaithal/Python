21cc79d7cbc5c53ae569aa04622239119d3e50c4
suma.aithal <aithalsuma65@gmail.com>
2026-05-03 17:55:25 +0530

    largest so far

---
+++ largest_number.py
@@ -0,0 +1,6 @@
+largest_so_far = -1
+for i in [9, 41, 12, 3, 74, 15]:
+    if i > largest_so_far:
+        largest_so_far = i 
+    print("largest so far", largest_so_far)
+print("After", largest_so_far)
