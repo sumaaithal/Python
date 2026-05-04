fdbb84c31c524b91ff8833bea099d13fe5a425a2
suma.aithal <aithalsuma65@gmail.com>
2026-05-04 10:35:50 +0530

    summing

--- Average.py
+++ Average.py
@@ -2,7 +2,7 @@ Count = 0
 Sum = 0
 print("Before", Count, Sum)
 for num in [9, 41, 12, 3, 74, 15]:
-    Count = Count + 1
-    Sum = Sum + num
+    Count = Count + 1 #counting the number of items in the list
+    Sum = Sum + num #runing total of the sum
     print(Count, Sum, num)
 print("After", Count, Sum, Sum/Count)
