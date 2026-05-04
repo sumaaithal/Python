ec8fbf3caa04a3d78a024cbd03c15669ac921a86
suma.aithal <aithalsuma65@gmail.com>
2026-05-04 11:41:13 +0530

    smallest

---
+++ smallest.py
@@ -0,0 +1,9 @@
+smallest = None 
+print("Before", smallest)
+for value in [9, 41, 12, 3, 74, 15]:
+    if smallest is None:
+        smallest = value
+    elif value < smallest:
+        smallest = value
+    print("Smallest so far", smallest)
+print("After", smallest)
