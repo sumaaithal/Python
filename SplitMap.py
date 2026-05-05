f75e9fce68d35195062763e34ff57c367bda6e7d
suma.aithal <aithalsuma65@gmail.com>
2026-05-05 21:09:55 +0530

    split map

---
+++ SplitMap.py
@@ -0,0 +1,6 @@
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+def split_title_and_name(person):
+    return person.split()[0]+""+person.split()[2]
+
+print(list(map(split_title_and_name, people)))
