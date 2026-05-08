6fe0a6c3857fe64dd45522e8ce78686ce2c1b175
suma.aithal <aithalsuma65@gmail.com>
2026-05-08 08:58:15 +0530

    bananas

---
+++ bananas.py
@@ -0,0 +1,7 @@
+word = 'Sam'
+if word > 'banana':
+    print('Your word,' + word + ', comes after banana.')
+elif word < 'banana':
+    print('Your word,' + word + ', comes before banana.')
+else:
+    print('All right, bananas.')
