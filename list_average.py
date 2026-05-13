82b45374768fdbcd84df41a0f6afd574dbc59b4f
suma.aithal <aithalsuma65@gmail.com>
2026-05-13 14:05:00 +0530

    average using numbers

---
+++ list_average.py
@@ -0,0 +1,10 @@
+numlist = list()
+while True:
+    inp = input("enter a number:")
+    if inp == "done":
+        break
+    value = float(inp)
+    numlist.append(value)
+
+average = sum(numlist) / len(numlist)
+print("average:", average)
