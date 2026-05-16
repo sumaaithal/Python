bfe4dbabfbaced3bb41672e45d8236861a670a2f
suma.aithal <aithalsuma65@gmail.com>
2026-05-16 09:37:09 +0530

    email parser 1

---
+++ email_parser1.py
@@ -0,0 +1,11 @@
+fh = open('mbox-short.txt')
+count = 0
+for line in fh:
+    line = line.strip()
+    wds = line.split()
+
+    if len(wds) < 3 or wds[0] != 'From':
+        continue
+    print(wds[1])
+    count += 1
+print("there are {} emails".format(count))
