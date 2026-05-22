b9caa047296de030a414bd2a3121cdedcc270891
suma.aithal <aithalsuma65@gmail.com>
2026-05-22 13:52:55 +0530

    eamil frequency

---
+++ email_freq_count.py
@@ -0,0 +1,23 @@
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+d = dict()
+handle = open(name)
+for line in handle:
+    line = line.strip()
+    wds = line.split()
+    if len(wds) < 3 or wds[0] != 'From':
+        continue
+    
+    else:
+        email = wds[1]
+        d[email] = d.get(email,0) + 1
+
+
+emailcount = None
+freqemail = None
+for email,counts in d.items():
+    if emailcount is None or counts > emailcount:
+        freqemail = email
+        emailcount = counts
+print(freqemail, emailcount)
