9b3fef9a85ad15b8e146bff5a7c8509a81f45dd3
suma.aithal <aithalsuma65@gmail.com>
2026-05-25 19:46:14 +0530

    regex

---
+++ find_search.py
@@ -0,0 +1,36 @@
+#find like search
+# find 
+
+# handle = open('mbox-short.txt')
+# for line in handle:
+#     line = line.rstrip()
+#     if line.find('From:') >= 0:
+#         print(line)
+
+# re.search 
+
+# import re 
+
+# handle = open('mbox-short.txt')
+# for line in handle:
+#     line = line.rstrip()
+#     if re.search('From:', line):
+#         print(line)
+
+#search like startswith
+#startswith
+
+# handle = open('mbox-short.txt')
+# for line in handle:
+#     line = line.rstrip()
+#     if line.startswith('From:'):
+#         print(line)
+
+#search
+
+# import re
+# handle = open('mbox-short.txt')
+# for line in handle:
+#     line = line.rstrip()
+#     if re.search('^From:', line):
+#         print(line)
