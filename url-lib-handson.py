81776e19475f1966c7410184bcc86158bce807b1
suma.aithal <aithalsuma65@gmail.com>
2026-06-10 09:47:05 +0530

    urllib

--- url-lib-handson.py
+++ url-lib-handson.py
@@ -1,4 +1,10 @@
 import urllib.request, urllib.parse, urllib.error
 
+counts = dict()
+
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-for line in fhand:    print(line.decode().strip())
+for line in fhand:  
+    words = line.decode().split() #byte to unicode, split into words
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1  
+print(counts)
