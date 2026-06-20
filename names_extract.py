8d79c65a1c9956bc679a8372176a604ce0596537
suma.aithal <aithalsuma65@gmail.com>
2026-06-20 09:22:24 +0530

    name parser

---
+++ names_extract.py
@@ -0,0 +1,26 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+#ignore ssl certification errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#read html
+url = input('Enter - ')
+count = int(input('Enter count: '))
+position = int(input('Enter position: '))
+
+for i in range(count+1):
+    print('Retrieving:', url)
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+
+    tags = soup('a')
+    if i == count:
+        break
+    tag = tags[position-1]
+    url = tag.get('href', None)
+print(url)
